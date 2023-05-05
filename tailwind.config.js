02048af4982295517b5ab98981338591cbd230b5
tuana <tuananh45468@gmail.com>
2023-05-05 14:14:07 +0700

    complete mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     colors: {
       violet: "hsl(257, 40%, 49%)",
       "soft-magenta": "hsl(300, 69%, 71%)",
+      white: "#fff",
     },
     screens: {
       mobile: "375px",
@@ -18,6 +19,10 @@ module.exports = {
       fontWeight: {
         regular: 400,
         "semi-bold": 600,
+      },
+      backgroundImage: {
+        "mobile-background": "url(/dist/images/bg-mobile.svg)",
+        "desktop-background": "url(/dist/images/bg-desktop.svg)",
       },
     },
   },
